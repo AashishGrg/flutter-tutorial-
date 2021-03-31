d01207c85896dd636ec413f8182e34eafe56fb3f
aashish <ashish.gurung@cdts.com.np>
2021-03-31 15:18:06 +0545

    Stateless widget practise

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'hello, Aashish!',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-            ),
+void main() =>
+    runApp(MaterialApp(
+        home:Home(),
+    ));
+
+//stateless widget practise : state of the widget cannot change over time.
+//stateful widget  contain widget can change over time.
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'hello, Aashish!',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click'),
-          backgroundColor: Colors.red[600],
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
