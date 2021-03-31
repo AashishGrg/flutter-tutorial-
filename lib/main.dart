17e019e164bed30de78f7edae398bf5e833bfa4e
aashish <ashish.gurung@cdts.com.np>
2021-03-31 15:06:27 +0545

    colors and fonts properties in widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('hello, ninjas!'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
-));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text(
+            'hello, Aashish!',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
+    ));
